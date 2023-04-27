dc3c0c6c77a9cea611d644452a7392907cacfd5b
vnme1 <vnme1112@naver.com>
2023-04-28 00:21:45 +0900

    real sixth

--- api-server/api/_controller/studentController.js
+++ api-server/api/_controller/studentController.js
@@ -1,38 +1,155 @@
-const db = require('../../plugins/mysql');
+const db = require("../../plugins/mysql");
 const TABLE = require("../../util/TABLE");
 const STATUS = require("../../util/STATUS");
 const { resData, currentTime, isEmpty } = require("../../util/lib");
+const moment = require("../../util/moment");
+
+//전체 row 갯수
+const getTotal = async () => {
+  try {
+    const query = `SELECT COUNT(*) AS cnt FROM ${TABLE.STUDENT}`;
+    const [[{ cnt }]] = await db.execute(query);
+    return cnt;
+  } catch (e) {
+    console.log(e.message);
+    return resData(STATUS.E300.result, STATUS.E300.resultDesc, currentTime());
+  }
+};
+
+// row 존재유무
+const getSelectOne = async (s_id) => {
+  // const getTotal = async function () {
+  try {
+    const query = `SELECT COUNT(*) AS cnt FROM ${TABLE.STUDENT} WHERE s_id=?`;
+    const values = [s_id];
+    const [[{ cnt }]] = await db.execute(query, values);
+    return cnt;
+  } catch (e) {
+    console.log(e.message);
+    return resData(STATUS.E300.result, STATUS.E300.resultDesc, moment().format('LT'));
+  }
+};
+
+// 페이징으로 가져오기
+const getList = async (req) => {
+  try {
+    // 마지막 id, len 갯수
+    const lastId = parseInt(req.query.lastId) || 0;
+    const len = parseInt(req.query.len) || 10;
+
+    let where = "";
+    if (lastId) {
+      // 0은 false
+      where = `WHERE s_id < ${lastId}`;
+    }
+    const query = `SELECT * FROM ${TABLE.STUDENT} ${where} order by s_id desc limit 0, ${len}`;
+    const [rows] = await db.execute(query);
+    return rows;
+  } catch (e) {
+    console.log(e.message);
+    return resData(STATUS.E300.result, STATUS.E300.resultDesc, currentTime());
+  }
+};
 
 const studentController = {
+  // create
+  create: async (req) => {
+    const { s_name, gender } = req.body;
+    if (isEmpty(s_name) || isEmpty(gender)) {
+      return resData(STATUS.E100.result, STATUS.E100.resultDesc, moment().format('LT'));
+    }
 
-    async getsTest(){
-        const query =  `SELECT * FROM vue.student`;
-        const [[rows]] = await db.execute(query);
-        return rows;
-    },
-
-    create: async (req) => {
-      const { s_name, gender } = req.body;
-      if (isEmpty(s_name) || isEmpty(gender)) {
-        return resData(STATUS.E100.result, STATUS.E100.resultDesc);
+    try {
+      const query = `INSERT INTO student (s_name, gender) VALUES (?,?)`;
+      const values = [s_name, gender];
+      const [rows] = await db.execute(query, values);
+      if (rows.affectedRows == 1) {
+        return resData(
+          STATUS.S200.result,
+          STATUS.S200.resultDesc,
+          moment().format('LT'),
+        );
       }
+    } catch (e) {
+      console.log(e.message);
+      return resData(STATUS.E300.result, STATUS.E300.resultDesc, moment().format('LT'));
+    }
+  },
 
-      try{
-        const query = `INSERT INTO student (s_name, gender) VALUES (?,?)`;
-        const values = [s_name, gender];
-        const [rows] = await db.execute(query, values);
+  // list
+  list: async (req) => {
+    // 화살표함수는 es6문법 this접근안됨
+    const totalCount = await getTotal();
+    const list = await getList(req);
+    if (totalCount > 0 && list.length) {
+      return resData(
+        STATUS.S200.result,
+        STATUS.S200.resultDesc,
+        moment().format('LT'),
+        { totalCount, list }
+      );
+    } else {
+      return resData(STATUS.S201.result, STATUS.S201.resultDesc, moment().format('LT'));
+    }
+  },
 
-        if (rows.affectedRows == 1) {
-          return resData(
-            STATUS.S200.result,
-            STATUS.S200.resultDesc
+  //update
+  update: async (req) => {
+    const { s_id } = req.params; // url /로 들어오는것
+    const { s_name, gender } = req.body;
+    if (isEmpty(s_id) || isEmpty(s_name) || isEmpty(gender)) {
+      return resData(STATUS.E100.result, STATUS.E100.resultDesc, moment().format('LT'));
+    }
 
-          );
-        }
-      } catch (e) {
-        console.log(e.message);
-        return resData(STATUS.E300.result, STATUS.E300.resultDesc);
+    try {
+      const query = `UPDATE ${TABLE.STUDENT} SET s_name =?, gender=? WHERE s_id= ?`;
+      const values = [s_name, gender, s_id];
+      const [rows] = await db.execute(query, values);
+      if (rows.affectedRows == 1) {
+        return resData(
+          STATUS.S200.result,
+          STATUS.S200.resultDesc,
+          moment().format('LT')
+        );
       }
-    },
-}
+    } catch (e) {
+      console.log(e.message);
+      return resData(STATUS.E300.result, STATUS.E300.resultDesc, moment().format('LT'));
+    }
+  },
+
+  delete: async (req) => {
+    const { s_id } = req.params; // url /로 들어오는것
+    if (isEmpty(s_id)) {
+      return resData(STATUS.E100.result, STATUS.E100.resultDesc, moment().format('LT'));
+    }
+    const cnt = await getSelectOne(s_id);
+    try {
+      if (!cnt) {
+        return resData(
+          STATUS.E100.result,
+          STATUS.E100.resultDesc,
+          moment().format('LT')
+        );
+      }
+      const query = `DELETE FROM ${TABLE.STUDENT} WHERE s_id = ?;`;
+      const values = [s_id];
+      const [rows] = await db.execute(query, values);
+      if (rows.affectedRows == 1) {
+        return resData(
+          STATUS.S200.result,
+          STATUS.S200.resultDesc,
+          moment().format('LT')
+        );
+      }
+    } catch (e) {
+      console.log(e.message);
+      return resData(STATUS.E300.result, STATUS.E300.resultDesc, moment().format('LT'));
+    }
+    return rows;
+  },
+
+
+};
+
 module.exports = studentController;
--- api-server/api/student.js
+++ api-server/api/student.js
@@ -1,14 +1,35 @@
 const router = require('express').Router();
 const studentController = require('./_controller/studentController');
-router.get('/', async (req,res)=>{
-    const result = await studentController.getsTest();
-    res.json(result);
-})
 
 // create
 router.post("/", async (req, res) => {
   const result = await studentController.create(req);
   res.json(result);
 });
+
+// list
+router.get('/', async (req,res)=>{
+  const result = await studentController.list(req);
+  res.json(result);
+})
+
+// update
+router.put('/:id', async (req,res)=>{
+  const result = await studentController.update(req);
+  res.json(result);
+})
+
+// delete
+router.delete('/:id', async (req,res)=>{
+  const result = await studentController.delete(req);
+  res.json(result);
+})
+
+// truncate, dummy insert
+router.post('/reset', async (req,res)=>{
+  const result = await studentController.reset(req);
+  res.json(result);
+})
+
 
 module.exports = router;
